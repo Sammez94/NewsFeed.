108d0e5c55c577523efe125ea2e2f7473fd7a6fd
Samuel <aremusamuel388@gl.commai>
2017-12-29 00:24:24 +0100

    .

--- app/src/main/java/com/samuellaxman/newsfeed/BusinessFragment.java
+++ app/src/main/java/com/samuellaxman/newsfeed/BusinessFragment.java
@@ -1,12 +1,25 @@
 package com.samuellaxman.newsfeed;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by USER on 12/27/2017.
  */
 
 public class BusinessFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootview=inflater.inflate(R.layout.sports_fragment,container,false);
+
+        return rootview;
+    }
+
     public BusinessFragment(){}
 
 }
--- app/src/main/java/com/samuellaxman/newsfeed/SportsFragment.java
+++ app/src/main/java/com/samuellaxman/newsfeed/SportsFragment.java
@@ -1,11 +1,28 @@
 package com.samuellaxman.newsfeed;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by USER on 12/27/2017.
  */
 
 public class SportsFragment extends Fragment {
-    public SportsFragment(){}
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootview=inflater.inflate(R.layout.sports_fragment,container,false);
+
+        return rootview;
+    }
+
+    public SportsFragment() {
+
+    }
 }
